a0c342ea1079c402298579b44c8bda723fe6c0bd
abdelrahman abdelnabi <abdoumohamed96@gmail.com>
2017-03-27 23:10:38 +0200

    update quicksort.cpp

--- QuickSort/quicksort.cpp
+++ QuickSort/quicksort.cpp
@@ -7,43 +7,11 @@
 
 #include <iostream>
 
-void sort(int array[]);
-void quickSort(int array[], int low, int high);
-int partition(int array[], int p, int r);
-int swap(int* a, int* b);
-
-void sort(int array[]) {
-	quickSort(array, 0, (sizeof(array)/sizeof(int)));
-}
+//void sort(int array[]);
+//void quickSort(int array[], int low, int high);
+//int partition(int array[], int p, int r);
+//void swap(int* a, int* b);
 
 
-void quickSort(int array[], int low, int high) {
-	if(low < high) {
-		int q = partition(array, low, high);
-		quickSort(array, low, q - 1);
-		quickSort(array, q + 1, high);
-	}
-
-}
 
 
-int partition(int array[], int p, int r) {
-	int x = array[r];
-	int i = p - 1;
-
-	for(int j = p; j < r-1; j++) {
-		if(array[j] <= x) {
-			i++;
-			swap(&array[i], &array[j]);
-		}
-	}
-	swap(&array[i+1], &array[r]);
-	return i + 1;
-}
-
-
-void swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
